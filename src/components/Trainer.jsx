90255bac852d3305b66f5eef68ff36239921ccec
itscel <144205012+itscel@users.noreply.github.com>
2023-12-05 15:54:51 +0800

    Add files via upload
    Changes in Dashboard.jsx and Trainer.jsx

--- src/components/Trainer.jsx
+++ src/components/Trainer.jsx
@@ -1,33 +1,35 @@
-import TrainerProfile from "../assets/trainer.jpeg";
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 function DropdownComponent() {
+  const handleDropdownToggle = () => {
+    const dropdown = document.getElementById('dropdown');
+    dropdown.classList.toggle('hidden');
+  };
+
   return (
-    <div className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+    <div
+      id="dropdown"
+      className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow-lg dark:bg-white dark:border-gray-700"
+      style={{
+        minWidth: '3in',
+        maxWidth: '4 in',
+        minHeight: '2in',
+        maxHeight: '3in',
+      }}
+    >
       <div className="flex flex-col items-center pb-10">
-        <img
-          className="mt-5 w-24 h-24 mb-3 rounded-full shadow-lg"
-          src={TrainerProfile}
-          alt="Bonnie image"
-        />
-        <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">
-          Trainer Name
-        </h5>
-        <span className="text-sm text-gray-500 dark:text-gray-400">
-          Expertise
-        </span>
+        <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">Trainer Name</h5>
+        <span className="text-sm text-gray-500 dark:text-gray-400">Expertise</span>
         <div className="flex mt-4 md:mt-6">
-          <a
-            href="#"
-            className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-          >
-            Book
-          </a>
-          <a
-            href="#"
-            className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-gray-900 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-700 dark:focus:ring-gray-700 ms-3"
-          >
-            Profile
-          </a>
+          <Link to="/addtrainer">
+            <a
+              href="#"
+              className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            >
+              Book
+            </a>
+          </Link>
         </div>
       </div>
     </div>
--- src/main pages/Dashboard.jsx
+++ src/main pages/Dashboard.jsx
@@ -7,13 +7,38 @@ import {Link} from 'react-router-dom';
 import Logo from "../assets/fitfusion.png";
 import logout from "../assets/logout.png";
 import profilePicture from "../assets/prfile.jpg";
+import Trainer from "./../components/Trainer"
 
 function Sidebar() {
   const [sidebarOpen, setSidebarOpen] = useState(false);
+  const [selectedOptions, setSelectedOptions] = useState({
+    intensity: '',
+    frequency: '',
+    duration: '',
+  });
 
   const handleSidebarToggle = () => {
     setSidebarOpen(!sidebarOpen);
   };
+
+  const intensityOptions = ['Easy', 'Moderate', 'Hard'];
+  const frequencyOptions = ['3', '4', '5', '6', '7'];
+  const durationOptions = ['5', '10', '15', '20', '25', '30'];
+
+  const handleSelectChange = (event, field) => {
+    setSelectedOptions({
+      ...selectedOptions,
+      [field]: event.target.value,
+    });
+  };
+
+  const scrollToEnrolledClasses = () => {
+    const enrolledClassesDiv = document.getElementById('enrolledclass');
+    if (enrolledClassesDiv) {
+      enrolledClassesDiv.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
+
 
   return (
     <>
@@ -61,9 +86,19 @@ function Sidebar() {
               <li>
                 <a
                   href="#"
-                  className="flex items-center p-3 text-gray-800 dark:text-white rounded-lg hover:bg-gray-100 dark:hover:bg-gray-800 group"
+                  className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
                 >
                   <span>Dashboard</span>
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  onClick={scrollToEnrolledClasses}
+                  className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
+                >
+                  {/* Notifications Icon */}
+                  <h1>Enrolled Classes</h1>
                 </a>
               </li>
             </ul>
@@ -73,7 +108,7 @@ function Sidebar() {
             {/* Logout section */}
             <Link to="/login"
               href="#"
-              className="flex items-center text-gray-800 dark:text-white"
+              className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
             >
               <img src={logout} alt="Logout" className="h-6 mr-2" />
               <span className="text-sm font-semibold">Log Out</span>
@@ -87,9 +122,87 @@ function Sidebar() {
           <h1 className="text-4xl font-bold text-gray-900 dark:text-gray-900">
             Choose a Workout
           </h1>
+          <div className="flex items-center space-x-4">
+            <div className="w-1/3">
+              <label htmlFor="intensity" className="mr-2">
+                <strong>Intensity: </strong>
+              </label>
+              <select
+                id="intensity"
+                value={selectedOptions.intensity}
+                onChange={(e) => handleSelectChange(e, 'intensity')}
+                className="w-full px-3 py-2 rounded-md border border-gray-300 focus:outline-none focus:border-blue-500"
+              >
+                <option value="">Choose</option>
+                {intensityOptions.map((option) => (
+                  <option key={option} value={option}>
+                    {option}
+                  </option>
+                ))}
+              </select>
+            </div>
+            {/* Frequency dropdown */}
+            <div className="w-1/3">
+              <label htmlFor="frequency" className="mr-2">
+                <strong>Frequency: </strong> <i>(days in a week)</i>
+              </label>
+              <select
+                id="frequency"
+                value={selectedOptions.frequency}
+                onChange={(e) => handleSelectChange(e, 'frequency')}
+                className="w-full px-3 py-2 rounded-md border border-gray-300 focus:outline-none focus:border-blue-500"
+              >
+                <option value="">Choose</option>
+                {frequencyOptions.map((option) => (
+                  <option key={option} value={option}>
+                    {option}
+                  </option>
+                ))}
+              </select>
+            </div>
+            {/* Duration dropdown */}
+            <div className="w-1/3">
+              <label htmlFor="duration" className="mr-2">
+                <strong>Duration: </strong><i>(minute per session)</i>
+              </label>
+              <select
+                id="duration"
+                value={selectedOptions.duration}
+                onChange={(e) => handleSelectChange(e, 'duration')}
+                className="w-full px-3 py-2 rounded-md border border-gray-300 focus:outline-none focus:border-blue-500"
+              >
+                <option value="">Choose</option>
+                {durationOptions.map((option) => (
+                  <option key={option} value={option}>
+                    {option}
+                  </option>
+                ))}
+              </select>
+            </div>
+          </div>
         </div>
         <div className="mt-4">
         </div>
+        <div className="flex flex-wrap gap-4 mt-4 justify-center">
+          {/* Container to display trainers side by side */}
+          {Array.from({ length: 50 }).map((_, index) => (
+            <div key={index}>
+              {/* Set the width as per your preference, in this case, 20% */}
+              <Trainer />
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div id="enrolledclass" className="p-4 sm:ml-64">
+        <div className="p-4 border border-gray-300 rounded-lg dark:border-gray-700">
+          <h1 className="text-4xl font-bold text-gray-900 dark:text-gray-900">
+            Enrolled Classes
+          </h1> 
+        </div>
+        <div className="mt-4">
+        </div>
+        
       </div>
     </>
   );
